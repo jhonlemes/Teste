6c834048874c967e31c78e5a6e708228279c34fd
rafoliveir <rafol@192.168.44.15>
2015-11-10 22:07:29 -0200

    10/11/15

--- src/Exercicio21/testeTelevisor.java
+++ src/Exercicio21/testeTelevisor.java
@@ -5,12 +5,20 @@ public class testeTelevisor {
 	public static void main(String[] args) {
 		Televisor televisor = new Televisor(null, null, null);
 		
-		televisor.setLigado(false);
+		televisor.setLigado(true);
 		televisor.setCanal(02);
 		televisor.setVolume(10);
 		
 		televisor.imprimir();
+		
+		
+			
+			
+		}
 
+	public void desligar(){
+		
+		
 	}
 
 }
